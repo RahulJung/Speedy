bf25b2c57a1201f8bd648a61f721b193ba66078d
RahulJung <rahuljungchauhan@gmail.com>
2021-06-30 05:21:24 -0400

    - MVP complete

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /\.css?/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(gif|svg|jpg|png)$/,
+        loader: "file-loader",
+      },
     ],
   },
   output: {
