b3290f5b4e824bab3538be1f4c1a480e7882e283
RahulJung <rahuljungchauhan@gmail.com>
2021-06-30 05:13:40 -0400

    - Data for carousel

---
+++ client/src/components/SliderData.jsx
@@ -0,0 +1,14 @@
+export const SliderData = [
+  {
+    message:
+      "2021 is the year Google is starting to measure Site Speed as a major ranking factor in SEO. All things being equal, if your site is slower than your competitors, you will fall behind on Google search. Reducing your chance of being organically found before your competitors.",
+  },
+  {
+    message:
+      "A fast site is sign of quality user experience. Your customers will stay on your site longer if the site speed is faster. A slow site will put them right into the hands of your competitors. They will also convert faster and bounce off the page less. Leaving them more time to find what they were looking for in your services.",
+  },
+  {
+    message:
+      "Ferocious’ Grademysite.com tool helps you gauge what your current page speed is and also identifies the steps you need to take to fix them. Test your site with GradeMySite.com today!",
+  },
+];
