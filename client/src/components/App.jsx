4910713a95ccf59da1114099a346ec2be8578a90
RahulJung <rahuljungchauhan@gmail.com>
2021-06-30 05:21:02 -0400

    - Add all components to App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import NavigationBar from "./NavigationBar.jsx";
-import laptop from "../../../laptop.png";
+import Form from "../components/Form.jsx";
+import Description from "./Description.jsx";
 import "../styles/styles.css";
+import FadeMessage from "./FadeMessage.jsx";
+import Slider from "./Slider.jsx";
+import { SliderData } from "./SliderData.jsx";
+import Footer from "./Footer.jsx";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,8 +16,18 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <NavigationBar />
-        <div className="frontImg">Hello</div>
+        <div className="navBar">
+          <NavigationBar />
+        </div>
+        <div className="frontImg">
+          <FadeMessage />
+        </div>
+        <div className="formContainer">
+          <Description />
+          <Form />
+        </div>
+        <Slider slides={SliderData} />
+        <Footer />
       </div>
     );
   }
