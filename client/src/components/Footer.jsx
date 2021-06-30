d7d7c5a79cf7176dd7f25d238fcd43d7a2cac09a
RahulJung <rahuljungchauhan@gmail.com>
2021-06-30 05:11:01 -0400

    - Create a footer component for the website

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <p>
+        © 2021 |<a href="https://ferociousmedia.com/">Ferocious Media.</a>
+        <a href="https://ferociousmedia.com/privacy-policy/">Privacy Policy</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
